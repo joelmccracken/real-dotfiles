55d4139fe9a872fa5380962c55f96d2cbf41c4e9
Joel McCracken <mccracken.joel@gmail.com>
2016-07-15 00:36:34 -0400

    add opening dired cleanup to status menu bar

--- bitbar/status.5s.sh
+++ bitbar/status.5s.sh
@@ -5,5 +5,6 @@ number=`cat ~/.redd-up/cache | wc -l`
 echo "Untidy: $number| color=red"
 
 echo ---
+echo "Open dired cleanup | bash=/Users/joel/bin/emacs-process-inbox terminal=false"
 
 cat ~/.redd-up/cache
