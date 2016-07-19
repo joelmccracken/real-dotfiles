6860bc6cfea86f2bcdc1256be87aeccf86eea4bb
Joel McCracken <mccracken.joel@gmail.com>
2016-07-19 17:42:10 -0400

    retitle display of status and display the last run of the status checker updater

--- bitbar/status.5s.sh
+++ bitbar/status.5s.sh
@@ -2,9 +2,10 @@
 
 number=`cat ~/.redd-up/cache | wc -l`
 
-echo "Untidy: $number| color=red"
+echo "Issues: $number| color=red"
 
 echo ---
 echo "Open dired cleanup | bash=/Users/joel/bin/emacs-process-inbox terminal=false"
-
+cat ~/.redd-up/last-run
+echo
 cat ~/.redd-up/cache
