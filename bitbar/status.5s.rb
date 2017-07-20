1769652f70c3eb3691d26176b1ae3d2a10329873
Joel McCracken <mccracken.joel@gmail.com>
2017-07-19 20:25:58 -0400

    add check for status command and fix

--- bitbar/status.5s.rb
+++ bitbar/status.5s.rb
@@ -1,18 +1,10 @@
 #!/usr/bin/env ruby
 
-require 'date'
 
 number=`cat ~/.redd-up/cache | wc -l`.strip
 
 puts "Issues: #{number} | color=red"
 puts "---"
-
-raw_string = File.read(File.expand_path '~/.ex_supervisor/last-backup-date')
-dt = DateTime.parse raw_string
-formatted_nice = dt.to_time.getlocal.strftime("%I:%M %p %a %d-%m-%Y")
-
-puts "last backup finished at: #{formatted_nice}"
 puts "Open dired cleanup | bash=/Users/joel/bin/emacs-process-inbox terminal=true"
-puts File.read(File.expand_path '~/.redd-up/last-run')
-puts ""
-puts File.read(File.expand_path '~/.redd-up/cache')
+
+puts `~/bin/status`
---
+++ check/status-command.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+bash -c 'status' > /dev/null
