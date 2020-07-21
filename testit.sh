0f69bcdacb89277288fd4d2bf2fecfffe8aafb5f
Joel McCracken <mccracken.joel@gmail.com>
2020-07-21 01:54:30 -0400

    try the nix-home-manager script

--- testit.sh
+++ testit.sh
@@ -4,4 +4,8 @@ set -euo pipefail
 echo hi this is test
 uname -a
 
-sh <(curl -L https://nixos.org/nix/install) --darwin-use-unencrypted-nix-store-volume
+# sh <(curl -L https://nixos.org/nix/install) --darwin-use-unencrypted-nix-store-volume
+
+# source $HOME/.nix-profile/etc/profile.d/nix.sh
+
+source install/nix-home-manager.sh
